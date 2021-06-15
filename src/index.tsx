2abf9bce1399819ee3fcafc6cb89c360704932f0
Keaton Sentak <ksentak01@gmail.com>
2021-06-15 19:48:55 -0400

    setup basic form in index

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
