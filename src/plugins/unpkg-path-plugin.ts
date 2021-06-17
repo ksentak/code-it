a2a19f745056b11c0904dcb18ec30f8f5f882c6e
Keaton Sentak <ksentak01@gmail.com>
2021-06-17 14:32:52 -0400

    generate unpkg url

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,9 +9,23 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
+        } 
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href
+          }
         }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+        
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -21,7 +35,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `
           };
