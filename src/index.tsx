1bd7a9686e82fc7433dbc90848a17fefdd48aa48
Keaton Sentak <ksentak01@gmail.com>
2021-06-17 14:58:57 -0400

    remove error warning

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,11 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window'
+      }
     });
 
     // console.log(result);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -31,8 +31,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react'
+              console.log(React, useState);
             `
           };
         }
