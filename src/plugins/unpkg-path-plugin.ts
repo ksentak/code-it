7bb6a8108bb110815a29a451c628cc3b659ee6f8
Keaton Sentak <ksentak01@gmail.com>
2021-06-17 14:50:21 -0400

    resolve nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
@@ -9,23 +9,19 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } 
+        }
 
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
-          }
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+          };
         }
 
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
-        }
-        
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' };
-        // }
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -35,16 +31,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `
           };
-        } 
-        const {data} = await axios.get(args.path)
+        }
+        const { data, request } = await axios.get(args.path);
         return {
           loader: 'jsx',
-          contents: data
-        }
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
       });
     }
   };
