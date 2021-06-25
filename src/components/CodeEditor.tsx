35d6f770dd0625c74260c454fb5b2fe7e66b2e56
Keaton Sentak <ksentak01@gmail.com>
2021-06-25 12:53:01 -0400

    configure editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height='500px' />;
+  return (
+    <MonacoEditor
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
+      language='javascript'
+      theme='dark'
+      height='500px'
+    />
+  );
 };
 
 export default CodeEditor;
