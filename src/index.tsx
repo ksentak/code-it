b5b8c93e85a0a526bac0f9189afce750c5ff26ef
Keaton Sentak <ksentak01@gmail.com>
2021-06-25 14:47:46 -0400

    refactor into preview component

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/CodeEditor';
+import Preview from './components/Preview';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
   const startService = async () => {
@@ -27,8 +28,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -40,38 +39,16 @@ const App = () => {
       }
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head>
-      </head>
-      <body>
-        <div id='root'></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch(err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          }, false)
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
-      <CodeEditor initialValue='const a = 1' onChange={(value) => setInput(value)} />
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <CodeEditor initialValue='' onChange={(value) => setInput(value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title='preview' ref={iframe} sandbox='allow-scripts' srcDoc={html} />
+      <Preview code={code} />
     </div>
   );
 };
