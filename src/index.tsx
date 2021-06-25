7ce12f36994f101f1b59a7343d6df708921cf191
Keaton Sentak <ksentak01@gmail.com>
2021-06-25 15:15:35 -0400

    create codecell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,11 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-import bundle from './bundler';
+import CodeCell from './components/CodeCell';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor initialValue='' onChange={(value) => setInput(value)} />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
