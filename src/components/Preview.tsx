be556995ce0d5e23ef6ea650f5ed085f588e4473
Keaton Sentak <ksentak01@gmail.com>
2021-06-28 14:16:22 -0400

    complete horizontal & vertical resizing

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -8,6 +8,9 @@ interface PreviewProps {
 const html = `
   <html>
     <head>
+      <style>
+        html { background-color: white; }
+      </style>
     </head>
     <body>
       <div id='root'></div>
--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -10,6 +10,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
   if (direction === 'horizontal') {
     resizableProps = {
+      className: 'resize-horizontal',
       minConstraints: [window.innerWidth * 0.2, Infinity],
       maxConstraints: [window.innerWidth * 0.75, Infinity],
       height: Infinity,
