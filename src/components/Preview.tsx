6b9b138fc369511d2e2df8d5e4c4de97068dd438
Keaton Sentak <ksentak01@gmail.com>
2021-07-08 14:05:36 -0400

    handle async errors

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -19,15 +19,20 @@ const html = `
           const root = document.querySelector('#root');
           root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
           console.error(err);
-        }
+        };
+
+        window.addEventListener('error', (event) => {
+          event.preventDefault();
+          handleError(event.error);
+        });
 
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
           } catch(err) {
-            handleError(err)
+            handleError(err);
           }
-        }, false)
+        }, false);
       </script>
     </body>
   </html>
