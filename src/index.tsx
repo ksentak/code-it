0e942c93b50f46f79b3d27cdf6299dacbc8ccc65
Keaton Sentak <ksentak01@gmail.com>
2021-07-21 23:43:59 -0400

    action creator return types

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom';
 // import CodeCell from './components/CodeCell';
 import TextEditor from './components/TextEditor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
