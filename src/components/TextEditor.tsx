b18af6fae1e1efee225d99401e8caa334284e79f
Keaton Sentak <ksentak01@gmail.com>
2021-07-21 22:27:43 -0400

    toggle md editor

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setIsEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => document.removeEventListener('click', listener, { capture: true });
+  }, []);
+
+  if (isEditing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setIsEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
     </div>
   );
 };
