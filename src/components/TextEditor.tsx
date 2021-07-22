e49e33b053ccb024a82976df7730f0faff9254d7
Keaton Sentak <ksentak01@gmail.com>
2021-07-21 22:41:19 -0400

    fix css name conflict

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import './TextEditor.css';
 
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
