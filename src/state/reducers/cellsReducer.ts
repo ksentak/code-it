68ec43e042ffa064d819ae3ee2d1e0b7ad0bbe2a
Keaton Sentak <ksentak01@gmail.com>
2021-07-22 09:06:15 -0400

    tyoescript return state

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -24,23 +24,23 @@ const reducer = produce((state: CellsState = initalState, action: Action) => {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      return;
+      return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order.filter((id) => id !== action.payload);
-      return;
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         id: randomId(),
@@ -57,7 +57,7 @@ const reducer = produce((state: CellsState = initalState, action: Action) => {
       } else {
         state.order.splice(foundIndex, 0, cell.id);
       }
-      return;
+      return state;
     default:
       return state;
   }
