777ac227a7b1f49279e18bac576782eb009cb31b
Keaton Sentak <ksentak01@gmail.com>
2021-08-11 00:22:14 -0400

    force visible styling

--- src/components/AddCell.tsx
+++ src/components/AddCell.tsx
@@ -3,13 +3,14 @@ import './AddCell.css';
 
 interface AddCellProps {
   nextCellId: string | null;
+  forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div className='add-cell'>
+    <div className={`add-cell ${forceVisible && 'force-visible'}`}>
       <div className='add-buttons'>
         {/* 
           Todo: Extract duplicates
