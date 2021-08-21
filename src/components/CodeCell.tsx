51b813de49068c1705176655b7b00c431be647ee
Keaton Sentak <ksentak01@gmail.com>
2021-08-21 14:40:16 -0400

    fix double import of react/react-dom

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -20,12 +20,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+
         const show = (value) => {
           const root = document.querySelector('#root');
 
           if (typeof value === 'object') {
             if (value.$$typeof && value.props) {
-              ReactDOM.render(value, root);
+              _ReactDOM.render(value, root);
             } else {
               root.innerHTML = JSON.stringify(value);
             }
